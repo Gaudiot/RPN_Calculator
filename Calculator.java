b261cee1e5e37d7ba6219f91448ce6597ea652a5
Gaudiot <victor.gaudiot@gmail.com>
2022-03-30 21:09:17 -0300

    read input from stk file

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 /*
 Exemplo de entrada:
@@ -23,33 +28,36 @@ public class Calculator {
 
         Stack<String> stack = new Stack<String>();
 
-        String str, op;
-        float num1, num2, result = 0;
-        while(scanner.hasNext()){
-            str = scanner.nextLine();
-            stack.push(str);
-            
-            if(isOperator(stack.peek())){
-                op = stack.pop();
-
-                num2 = Float.parseFloat(stack.pop());
-                num1 = Float.parseFloat(stack.pop());
-
-                if(op.equals("+")){
-                    result = num1 + num2;
+        try(BufferedReader br = Files.newBufferedReader(Paths.get("Calc1.stk"))){
+            String str, op;
+            float num1, num2, result = 0;
+            while((str = br.readLine()) != null){
+                stack.push(str);
+                
+                if(isOperator(stack.peek())){
+                    op = stack.pop();
+    
+                    num2 = Float.parseFloat(stack.pop());
+                    num1 = Float.parseFloat(stack.pop());
+    
+                    if(op.equals("+")){
+                        result = num1 + num2;
+                    }
+                    if(op.equals("*")){
+                        result = num1 * num2;
+                    }
+                    if(op.equals("-")){
+                        result = num1 - num2;
+                    }
+                    if(op.equals("/")){
+                        result = num1 / num2;
+                    }
+    
+                    stack.push(String.valueOf(result));
                 }
-                if(op.equals("*")){
-                    result = num1 * num2;
-                }
-                if(op.equals("-")){
-                    result = num1 - num2;
-                }
-                if(op.equals("/")){
-                    result = num1 / num2;
-                }
-
-                stack.push(String.valueOf(result));
             }
+        }catch (IOException e){
+            System.err.format("IOException: %s%n", e);
         }
 
         System.out.println(stack.pop());
